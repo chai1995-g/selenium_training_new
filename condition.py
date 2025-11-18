6a27d7021038eea2b794b06f4c5d5e6f15c6c6ce
chai1995-g <chaithrag1212@gmail.com>
2025-11-18 19:44:57 +0530

    keepüing it same

---
+++ condition.py
@@ -0,0 +1,10 @@
+a = 9
+b = 2
+c = 3
+
+if (a > b) and (a > c):
+  print(f"{a} is a greater number")
+elif (a < b) and (a < c):
+    print(f"{b}is a greater number")
+else:
+    print(f"{c} is a greater number")
