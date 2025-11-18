ed6fbb09f867fc37dd0e6dfb64825602bea62e81
chai1995-g <chaithrag1212@gmail.com>
2025-11-18 19:46:43 +0530

    c has been updated as g

--- condition.py
+++ condition.py
@@ -1,10 +1,10 @@
 a = 9
 b = 2
-c = 3
+g = 3
 
-if (a > b) and (a > c):
+if (a > b) and (a > g):
   print(f"{a} is a greater number")
-elif (a < b) and (a < c):
+elif (a < b) and (a < g):
     print(f"{b}is a greater number")
 else:
-    print(f"{c} is a greater number")
+    print(f"{g} is a greater number")
